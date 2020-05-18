507d0862cca626820908c4d63f92f15af32f1a57
efdali <efdal_incesu@hotmail.com>
2020-05-19 00:13:40 +0300

    server url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:5000/api";
 // export const USER_IMAGE_URL = "http://localhost:5000/user/";
-// export const BASE_URL = 'https://seyirlik-api.herokuapp.com';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://seyirlik-api.herokuapp.com';
+// export const BASE_URL = 'http://localhost:5000';
 export const API_URL = `${BASE_URL}/api`;
 export const USER_IMAGE_URL = `${BASE_URL}/user/`;
